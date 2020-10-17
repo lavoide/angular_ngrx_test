2f420d0425ed04a2b93f3da9c37189bef0c1ad7f
lavoide <lavoidde@gmail.com>
2020-10-17 19:41:23 +0300

    done maybe?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,10 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-
-import { AppComponent } from "./app.component";
-import { ProductComponent } from "./products/components/product/product.component";
-import { ProductAddComponent } from "./products/components/product-add/product-add.component";
-import { ProductEditComponent } from "./products/components/product-edit/product-edit.component";
-import { ProductListComponent } from "./products/components/product-list/product-list.component";
-
-import { AppRoutingModule } from "./app-routing.module";
-import { SharedModule } from "./shared/shared.module";
-import { ProductsModule } from "./products/products.module";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { ProductsModule } from './products/products.module';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
